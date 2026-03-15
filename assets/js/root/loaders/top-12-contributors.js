02d0638caaada20919798bb6015f5fc5a44e5942
VIRATI AKIRANANDHAN REDDY <viratiaki53@gmail.com>
2026-03-15 20:01:38 +0530

    feat: Implement loadContributors function to dynamically display top contributors from external JSON

---
+++ assets/js/root/loaders/top-12-contributors.js
@@ -0,0 +1,28 @@
+async function loadContributors() {
+  const res = await fetch("https://cdn.jsdelivr.net/gh/notfoundpages/v1.notfoundpages.dynamic@main/contributors/top-12-contributors.json");
+  const users = await res.json();
+
+  const container = document.getElementById("contributors-container");
+
+  users.forEach(u => {
+
+    const card = `
+    <a href="${u.github}" class="contributor-card">
+
+      <img src="${u.avatar}" class="contrib-avatar">
+
+      <div class="contrib-name" title="${u.name}">
+        ${u.name}
+      </div>
+
+      <div class="contrib-stats">
+        ${u.contributions} Contributions
+      </div>
+
+    </a>`;
+
+    container.insertAdjacentHTML("beforeend", card);
+  });
+}
+
+loadContributors();
